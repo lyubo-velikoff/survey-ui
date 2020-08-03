8fd9cefafdcf105d58e1c45fc7abdef56df39372
Lyubo Velikov <lyubo.velikov@chameleontechnology.co.uk>
2020-08-03 18:20:15 +0100

    refactor: Cheeky set env variable.

--- scripts/build.js
+++ scripts/build.js
@@ -1,8 +1,9 @@
-'use strict';
+
 
 // Do this as the first thing so that any code reading it knows the right env.
 process.env.BABEL_ENV = 'production';
 process.env.NODE_ENV = 'production';
+process.env.REACT_APP_API_URL = 'https://lyubo-survey-backend.herokuapp.com';
 
 // Makes the script crash on unhandled rejections instead of silently
 // ignoring them. In the future, promise rejections that are not handled will
